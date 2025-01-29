475cbab6c825fcda116243fd08453447492dd04a
Diego Silvera <diegosc1313@gmail.com>
2025-01-29 18:55:56 -0500

    Add select_rows_star to classes_functions.py

--- aux_functions/classes_functions.py
+++ aux_functions/classes_functions.py
@@ -39,3 +39,26 @@ def labels_per_classes(labels_path, output_path):
         with open(output_path + f"/particles_class_{class_}.pkl", "wb") as file:
             pickle.dump(list_, file)
     return
+
+def select_rows_star(input_path, output_path, indexes):
+    '''
+    Función que crea archivos .star a partir del .star original y una lista con los índices de las partículas a utilizar
+    Input: Path al .star original, Path para guardar el .star de salida e índices de partículas a utilizar
+    Output: Archivo .star solo con las partículas a utilizar
+    Ejemplo de uso:
+    with open('./particles_per_classes/particles_class_4.pkl', 'rb') as file:
+        particles_0 = pickle.load(file)
+    particles_0 = [x + 12 for x in particles_0] #Desplazamiento por líneas de metadata
+    select_rows_star('Parameters.star', 'Cluster4_Parameters.star', particles_0)
+    OJO: Si cambia la cantidad de líneas que ocupa la metadata, cambia la función
+    '''
+    with open(input_path, 'r') as f:
+        lines = f.readlines()
+    metadata  = lines[:13] #13 lineas de metadata antes de ir particula por particula
+    # Extraer solo las líneas con los índices especificados
+    selected_lines = [line for i, line in enumerate(lines) if i in indexes] 
+    #selected_lines_ = [x + const_ for x in selected_lines]
+    final_lines = metadata + selected_lines
+    with open(output_path, 'w') as f: #Si ya había otro archivo con el mismo nombre lo sobreescribe
+        f.writelines(final_lines)
+    return
